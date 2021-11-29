68b3977103778adf30db27e23858e4b67f0fc04a
Higor braga <higorbraga00@gmail.com>
2021-11-29 14:12:17 -0300

    projeto criado

---
+++ src/app/modules/gerenciamento-efetivo/gerenciamento-efetivo.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GerenciamentoEfetivoRoutingModule } from './gerenciamento-efetivo-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GerenciamentoEfetivoRoutingModule
+  ]
+})
+export class GerenciamentoEfetivoModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  BASE_API_URL:  'http://localhost:8080/',
+  KEYCLOAK_URL: 'http://localhost:8190/auth',
+  KEYCLOAK_REALM: 'FAB',
+  KEYCLOAK_CLIENT_ID: 'controle-efetivo-fe',
+  KEYCLOAK_REDIRECT_URI:
+    'http://localhost:4200/controle-efetivo/assets/silent-check-sso.html',
+  FRONT_URL: 'http://localhost:4200/controle-efetivo',
+  SHOW_LOGGING_ROUTES: true,
+};
+

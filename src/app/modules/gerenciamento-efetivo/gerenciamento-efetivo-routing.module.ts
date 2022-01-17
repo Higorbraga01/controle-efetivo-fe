44649d31153b880bb32e4035b62edd6403c94cd1
Higor braga <higorbraga00@gmail.com>
2022-01-17 13:14:17 -0300

    Implementa menu de navegaçao

--- src/app/modules/gerenciamento-efetivo/gerenciamento-efetivo-routing.module.ts
+++ src/app/modules/gerenciamento-efetivo/gerenciamento-efetivo-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
     component: EfetivoConsultaContainerComponent,
   },
   {
-    path: 'cadastro',
+    path: 'cadastro/pessoa',
     // canActivate: [AuthGuard],
     // canActivateChild: [AuthGuard],
     component: EfetivoCadastroContainerComponent,

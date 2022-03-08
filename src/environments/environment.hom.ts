a3ae959328bdfb1d9b3c3e2f314b3fb55d48ce0a
Higor braga <higorbraga00@gmail.com>
2022-03-08 15:21:54 -0300

    Adiciona environment de homologacao

---
+++ src/environments/environment.hom.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  homolog: true,
+  development: false,
+  version: '1.0.0',
+
+  KEYCLOAK_URL: 'https://auth.homolog.ccarj.intraer/auth/',
+  KEYCLOAK_REALM: 'FAB',
+  KEYCLOAK_CLIENT_ID: 'controle-efetivo-fe',
+
+  API_URL:
+    'https://api.servicos.homolog.ccarj.intraer/controle-efetivo-api/api'
+};
